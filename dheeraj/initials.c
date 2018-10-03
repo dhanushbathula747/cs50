a6f1d6d84651620cc238d133c6f74dbc3fff0f79
balaramakrishna245 <43821066+balaramakrishna245@users.noreply.github.com>
2018-10-03 21:20:45 +0530

    Hacktoberfest

--- dheeraj/initials.c
+++ dheeraj/initials.c
@@ -7,17 +7,17 @@ void main(void)
     string s = get_string();
 
     printf("%c", toupper(s[0]));
-    for(int i=0;i<strlen(s);i++)
+    for(int i=0;i<strlen(s);i++) //Checking the Condition.
     {
         if(s[i] == ' ')
         {
             if(s[i+1] >= 'a' && s[i+1]<='z')
             {
-                printf("%c", toupper(s[i+1]));
+                printf("%c", toupper(s[i+1])); //Prints the final answer.
             }
             else
             {
-                printf("%c",s[i+1]);
+                printf("%c",s[i+1]);//Else prints the other answer.
             }
 
         }
